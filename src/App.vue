4f4b1ff114d1c81a20e0a50ef5244b3adcf404bd
GianbaBotassi <gianba89@gmail.com>
2023-06-07 12:12:16 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from "./store.js";
+
+import AppSearch from './components/AppSearch.vue';
+
+export default {
+  components: {
+    AppSearch
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <AppSearch />
+</template>
+
+<style scoped lang="scss">
+@use 'styles/general' as *;
+@use 'styles/partials/variables' as *;
+</style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default {
+    name: "AppSearch"
+}
+</script>
+
+<template>
+    <div>
+        ciaodfadfafddfsoo
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
